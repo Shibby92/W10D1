ca0172272a96e0388cf373458f8d29e93f72a98a
Shibby92 <haris_000@hotmail.com>
2015-01-20 17:56:35 +0100

    Initial commit

--- src/Streaming.java
+++ src/Streaming.java
@@ -1,37 +1,30 @@
 import java.io.*;
-import java.nio.CharBuffer;
 
 public class Streaming {
 
 	public static void main(String[] args) {
-		InputStream dis = new DataInputStream(System.in);
 		Reader read = new InputStreamReader(System.in);
-		BufferedReader br= new BufferedReader(read);
-		BufferedInputStream bis= new BufferedInputStream(dis);
-		byte[] buffer = new byte[4];
-		CharBuffer javaCharBuffer= CharBuffer.allocate(4);
-		int numBytesRead;
-		try {
-//			dis.mark(4);
-//			while ((numBytesRead = dis.read(buffer)) >= 0) {
-//				System.out.println("procitali: " + numBytesRead + " bytea\n"
-//						+ new String(buffer));
-//				for(byte meh: buffer){
-//					meh=0;
-//				}
-//				dis.reset();
-//			while(read.read(javaCharBuffer)>=0){
-//				
-//			}
-			String line="";
-			while((line = br.readLine())!=null){
-				System.out.println(line);
+		BufferedReader br = new BufferedReader(read);
+		File f = new File("/Users/harisarifovic/Desktop/osoba");
+		PrintWriter pw = new PrintWriter(System.out);
+		Person test= new Person ("Haris","Arifovic",22);
+		
+		try (OutputStream os= new FileOutputStream (f);
+				DataOutputStream dos= new DataOutputStream(os) ){
+			dos.writeUTF(test.toString());
+			String line = "";
+			StringBuilder sb = new StringBuilder();
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
 			}
-			System.out.println("END");
+			pw.println((sb.toString()));
+			pw.flush();
+			pw.println(test);
+			pw.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 
 }
