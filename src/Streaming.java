7a15c827425b8b4f400bc7f9027cae608180d762
Shibby92 <haris_000@hotmail.com>
2015-01-21 11:43:30 +0100

    Dodani komentari

--- src/Streaming.java
+++ src/Streaming.java
@@ -1,5 +1,11 @@
 import java.io.*;
 
+/**
+ * Vjezbanje sa file-ovima
+ * 
+ * @author harisarifovic
+ *
+ */
 public class Streaming {
 
 	public static void main(String[] args) {
@@ -7,13 +13,20 @@ public class Streaming {
 		BufferedReader br = new BufferedReader(read);
 		File f = new File("/Users/harisarifovic/Desktop/osoba");
 		PrintWriter pw = new PrintWriter(System.out);
-		Person test= new Person ("Haris","Arifovic",22);
-		
-		try (OutputStream os= new FileOutputStream (f);
-				DataOutputStream dos= new DataOutputStream(os) ){
+		Person test = new Person("Haris", "Arifovic", 22);
+
+		try (OutputStream os = new FileOutputStream(f);
+				DataOutputStream dos = new DataOutputStream(os)) {
+			/*
+			 * Upisuje Person u file
+			 */
 			dos.writeUTF(test.toString());
 			String line = "";
 			StringBuilder sb = new StringBuilder();
+			/**
+			 * * Program omogucava da korisnik unosi stringove koliko hoce puta,
+			 * tek kad pritisne ctrl+d treba da ispise sve te stringove
+			 */
 			while ((line = br.readLine()) != null) {
 				sb.append(line);
 			}
@@ -24,7 +37,7 @@ public class Streaming {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
